d0c6d82f7739b26633ea3a51ea5948706a359139
panchepanevski <pance.panevski@gmail.com>
2019-12-01 16:30:24 +0100

    fix style on tasks

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,8 +3,7 @@ import styled from "@emotion/styled";
 
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
+  align-items: center;
   width: 95%;
   height: 100%;
   padding: 10px;
@@ -17,25 +16,29 @@ const Container = styled.div`
 `;
 
 const Title = styled.div`
+  flex-grow: 1;
   padding: 3px;
   border-radius: 5px;
 
-  &:hover {
-    font-weight: 900;
-    padding: 3px;
-    cursor: pointer;
-    background-color: ${props => props.theme.default.secondary};
-  }
+  &:hover,
   &:active {
-    border: 1px solid #44bd32;
-    transition: 0.3s;
-    background-color: ${props => props.theme.default.secondary};
+    color: #e84118;
+    cursor: pointer;
   }
 `;
 
+const StatusBox = styled.div`
+  font-size: 1rem;
+  align-items: center;
+  align-content: center;
+`;
+
 const Remove = styled.div`
-  height: 25px;
-  width: 25px;
+  height: 20px;
+  width: 20px;
+  background-color: #e84118;
+  margin-left: 10px;
+  border-radius: 15px;
 `;
 
 export default function Task({ id, status, title, detail }) {
@@ -45,13 +48,10 @@ export default function Task({ id, status, title, detail }) {
   const Detail = styled.div`
     display: ${isClicked ? "block" : "none"};
     height: 100%;
-    width: 98%;
-    width: 95%;
-    height: 100%;
+    width: 90%;
     margin: 0px auto;
-    background: #fff;
     color: #353b48;
-    font-size: 1rem;
+    font-size: 0.9rem;
     font-weight: 800;
     background: transparent;
   `;
@@ -73,14 +73,14 @@ export default function Task({ id, status, title, detail }) {
     <>
       <Container>
         <Title onClick={() => setIsClicked(!isClicked)}>{title}</Title>
-        <div>{status}</div>
+        <StatusBox>{status}</StatusBox>
         <Remove
           onClick={() => {
             setRemove(id);
             window.location.reload();
           }}
         >
-          <svg width="25" height="25" viewBox="0 0 24 24">
+          <svg width="20" height="20" fill="#fff" viewBox="0 0 24 24">
             <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
             <path d="M0 0h24v24H0z" fill="none" />
           </svg>
